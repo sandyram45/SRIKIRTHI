baba1b10649635d0b9b892ac75ae6e1e9929224d
sandyram45 <santhoshram456@gmail.com>
2022-06-09 19:15:53 +0530

    hosts

--- hpgas/settings.py
+++ hpgas/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['srikirthi.herokuapp.com/']
+ALLOWED_HOSTS = ['srikirthi.herokuapp.com']
 
 
 # Application definition
